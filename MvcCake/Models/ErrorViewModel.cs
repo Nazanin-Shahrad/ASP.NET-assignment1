e19fc2af70fc09ffa828177fc340eaa55ef028ef
nazaninshahrad <nazanin.shahrad@gmail.com>
2021-10-14 10:32:37 -0400

    add about us and product pages

---
+++ MvcCake/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MvcCake.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

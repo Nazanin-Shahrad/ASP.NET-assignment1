1aac7774f247c829cfdf7eb4ced9b36b23d49dca
nazaninshahrad <nazanin.shahrad@gmail.com>
2021-10-15 21:46:38 -0400

    seeddata

---
+++ MvcCake/Models/SeedData.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+namespace MvcCake.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcCakeContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcCakeContext>>()))
+            {
+                // Look for any movies.
+                if (context.Cake.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Cake.AddRange(
+                    new Cake
+                    {
+                        ProductName = "Birthday1 Cake",
+                        ProductionDate = DateTime.Parse("2021-10-10"),
+                        Comment = "Chocolate cake with chocolate mouse",
+                        Weight = 5,
+                        Price = 7.99M
+                    },
+
+                    new Cake
+                    {
+                        ProductName = "Wedding Cake",
+                        ProductionDate = DateTime.Parse("2021-2-12"),
+                        Comment = "3 tires Chocolate cake with chocolate mouse",
+                        Weight = 2,
+                        Price = 20
+                    },
+
+                    new Cake
+                    {
+                        ProductName = "Halloween Cookie",
+                        ProductionDate = DateTime.Parse("2021-09-12"),
+                        Comment = "Vanilla Cookie",
+                        Weight = 1,
+                        Price =300
+                    },
+
+                    new Cake
+                    {
+                        ProductName = "birthday2 Cake",
+                        ProductionDate = DateTime.Parse("2021-10-01"),
+                        Comment = "vanilla cake",
+                        Weight = 5,
+                        Price = 380
+                    },
+
+                    new Cake
+                    {
+                        ProductName = "wedding2 Cake",
+                        ProductionDate = DateTime.Parse("2021-10-01"),
+                        Comment = "chocolate cake",
+                        Weight = 1,
+                        Price = 200
+                    },
+
+
+                    new Cake
+                    {
+                        ProductName = "birthday3 Cake",
+                        ProductionDate = DateTime.Parse("2021-10-01"),
+                        Comment = "vanilla cake",
+                        Weight = 2,
+                        Price = 290
+                    },
+
+
+                    new Cake
+                    {
+                        ProductName = "breakfast1 Cake",
+                        ProductionDate = DateTime.Parse("2021-10-01"),
+                        Comment = "carret cake",
+                        Weight = 5,
+                        Price = 278
+                    },
+
+
+                    new Cake
+                    {
+                        ProductName = "breakfast2 Cake",
+                        ProductionDate = DateTime.Parse("2021-10-01"),
+                        Comment = "chocolate cake",
+                        Weight = 5,
+                        Price = 380
+                    },
+
+
+                    new Cake
+                    {
+                        ProductName = "breakfast3 Cake",
+                        ProductionDate = DateTime.Parse("2021-10-01"),
+                        Comment = "vanilla cake",
+                        Weight = 1,
+                        Price = 380
+                    }
+                ) ;
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- MvcCake/Program.cs
+++ MvcCake/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using MvcCake.Models;
+
 
 namespace MvcCake
 {
@@ -13,7 +16,25 @@ namespace MvcCake
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -24,3 +45,7 @@ namespace MvcCake
                 });
     }
 }
+
+
+
+
